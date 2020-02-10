b8f3a67deebf254c076de138472a4b018c661ab0
BrandonTremain <tremainb5862@my.uwstout.edu>
2020-02-10 13:58:22 -0600

    Added a script for gaining and losing money

---
+++ Assets/Scripts/Money_System.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Money_System : MonoBehaviour
+{
+    public int bank;
+    public Text bankText;
+
+    void Start()
+    {
+        bank = 50;
+        bankText.text = bank.ToString();
+    }
+
+    public void addBank(int bankToAdd)
+    {
+        bank += bankToAdd;
+        bankText.text = bank.ToString();
+    }
+
+    public void subtractBank(int bankToSubtract)
+    {
+        if (bank - bankToSubtract < 0)
+        {
+            // Add to it Later
+        }
+        else
+        {
+            bank -= bankToSubtract;
+            bankText.text = bank.ToString();
+        }
+    }
+}
