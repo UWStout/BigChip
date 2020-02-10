4d29c224885ff08a290b119c24472e5c5774b721
BrandonTremain <tremainb5862@my.uwstout.edu>
2020-02-10 12:59:51 -0600

    Added a Play Button for the Menu

---
+++ Assets/Scripts/Start_Button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Button : MonoBehaviour
+{
+    public void beginGame(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
