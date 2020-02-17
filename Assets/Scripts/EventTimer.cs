e583759ac88f2265c7f9fdaef4c656e38d939921
weryc1404 <weryc1404@my.uwstout.edu>
2020-02-17 13:34:38 -0600

    Start of the timer

---
+++ Assets/Scripts/EventTimer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTimer
+{
+    private int numOfEvents = 0;
+    private Action action;
+    private float timer;
+    private bool isFinished;
+    
+
+    public EventTimer(Action action, float timer)
+    {
+        this.action = action;
+        this.timer = timer;
+    }
+
+    public void Update()
+    {
+        if (!isFinished)
+        {
+            timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    //trigger action
+                    action();
+                    EndEvents(numOfEvents);
+                }
+        }
+        
+    }
+
+    private void EndEvents(int numOfEvents)
+    {
+        numOfEvents++;
+        isFinished = true;
+    }
+
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private EventTimer functionTimer;
+
+    private void Start()
+    {
+        functionTimer = new EventTimer(TestingAction, 3f);
+    }
+
+    private void Update()
+    {
+        functionTimer.Update();
+    }
+
+    private void TestingAction()
+    {
+        Debug.Log("Testing");
+    }
+
+}
