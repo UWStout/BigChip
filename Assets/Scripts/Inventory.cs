686f8afa2ccabf5411562c227b01a57b7ad65244
BrandonTremain <tremainb5862@my.uwstout.edu>
2020-02-19 12:44:00 -0600

    Made the Inventory have Correct math

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -15,24 +15,7 @@ public class Inventory : MonoBehaviour
 
     void Start()
     {
-        //initialize the array with a size
-        //unity doesnt like to change size
-        //we dont need to so we should be ok
-        //currently hardcode to set values into the array
-        //cookies.SetValue(8, 0);
-        //cookies.SetValue(6, 1);
-        //cookies.SetValue(7, 2);
-        //cookies.SetValue(5, 3);
-        //cookies.SetValue(3, 4);
-        //cookies.SetValue(9, 5);
 
-        //hardcoded to give each cookie a specific price
-        //price.SetValue(5, 0);
-        //price.SetValue(10, 1);
-        //price.SetValue(10, 2);
-        //price.SetValue(8, 3);
-        //price.SetValue(8, 4);
-        //price.SetValue(5, 5);
     }
 
 
@@ -44,118 +27,27 @@ public class Inventory : MonoBehaviour
     // error msg will print if an index is too high
     public void ChangeValue(int index, int change)
     {
-        //if (index == 1)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[0] < 0)
-        //        {
-        //            change = cookies[0];
-        //        }
-        //        cookies[0] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[0] += change;
-        //    }
-
-        //}else if(index == 2)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[1] < 0)
-        //        {
-        //            change = cookies[1];
-        //        }
-        //        cookies[1] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[1] += change;
-        //    }
-        //}
-        //else if (index == 3)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[2] < 0)
-        //        {
-        //            change = cookies[2];
-        //        }
-        //        cookies[2] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[2] += change;
-        //    }
-        //}
-        //else if (index == 4)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[3] < 0)
-        //        {
-        //            change = cookies[3];
-        //        }
-        //        cookies[3] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[3] += change;
-        //    }
-        //}
-        //else if (index == 5)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[4] < 0)
-        //        {
-        //            change = cookies[4];
-        //        }
-        //        cookies[4] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[4] += change;
-        //    }
-        //}
-        //else if (index == 6)
-        //{
-        //    if (change < 0)
-        //    {
-        //        if (change - cookies[5] < 0)
-        //        {
-        //            change = cookies[5];
-        //        }
-
-        //        cookies[5] -= change;
-        //    }
-        //    else if (change > 0)
-        //    {
-        //        cookies[5] += change;
-        //    }
-        //}
-
         if (index >= 1 && index <= 6)
         {
             if (change < 0)
             {
                 if (cookies[index - 1] + change < 0) // cookies[x] is positive, change is negative, positive plus negative means subtraction
                 {
-                    change = -(cookies[index - 1]); // Changes change to be whatever the inventory total is, making change a positive number
+                    change = cookies[index - 1]; // Changes change to be whatever the inventory total is, making change a positive number
                     cookies[index - 1] = 0; // This should always equal zero
                 }
                 else
                 {
                     cookies[index - 1] += change; // Change here is still negative, adding it to the inventory will result in a smaller number, but will still be greater than zero
-                }  
+                    change = -(change); // Sets the change to positive to work with the money update
+                }
+                bank = price[index - 1] * change;
+                UpdateBank();
             }
             else if (change >= 0)
             {
-                cookies[index - 1] += change; // Gaining inventory. Just add it on
+                cookies[index - 1] += change; // Gaining inventory. Just add it on. No call to bank.
             }
-            bank = price[index - 1] * change;
-            UpdateBank();
         }
         else if (index == 7)
         {
