e6a6d1c22e0d52c679e310e44702bca9a9287462
weryc1404 <weryc1404@my.uwstout.edu>
2020-02-22 15:24:13 -0600

    Current Build

---
+++ Assets/Scripts/Scrolling parts.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling_parts : MonoBehaviour
+{
+    public float parallaxEffect;
+    public GameObject cam;
+    private float length, startpos;
+    private Vector2 screenBounds;
+
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        screenBounds = cam.screenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+    }
+
+    private void FixedUpdate()
+    {
+        
+        if (screenBounds)
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Scrolling_parts.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrollingparts : MonoBehaviour
+{
+    public float parallaxEffect;
+    public GameObject cam;
+    private float length, startpos;
+
+
+    private void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    private void FixedUpdate()
+    {
+        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        transform.position = new Vector3((startpos + dist) * 5, transform.position.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+
+}
