cba451da3bf8d751e327e271b1eed118f21acbcf
weryc1404 <weryc1404@my.uwstout.edu>
2020-02-23 17:58:31 -0600

    Rex Animation Correct

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public Rigidbody2D rex;
     bool moving;
+    public Animator anim;
 
 
     private void Start()
@@ -18,18 +19,20 @@ public class PlayerMovement : MonoBehaviour
 
     public void IsHeMoving()
     {
-        Debug.Log("function check");
+        //Debug.Log("function check");
         if (moving == true)
         {
-            Debug.Log("Should stop moving");
+           // Debug.Log("Should stop moving");
             rex.velocity = new Vector3(0, 0, 0);
             moving = false;
+            anim.StartPlayback();
         }
         else
         {
-            Debug.Log("Should start moving");
+           // Debug.Log("Should start moving");
             rex.velocity = new Vector3(speed, 0, 0);
             moving = true;
+            anim.StopPlayback();
         }
     }
 }
