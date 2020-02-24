8cd6fbc17137df34053efe7f87a5d543879d3b7e
BrandonTremain <tremainb5862@my.uwstout.edu>
2020-02-23 18:52:50 -0600

    Check Description
    1. Updated the Prefabs to look Better
    2. Wrote a script to spawn houses at equal distances
    3. Some small tweaks to the scene

---
+++ Assets/Scripts/RandomPrefabSpawn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPrefabSpawn : MonoBehaviour
+{
+    public GameObject prefab1, prefab2, prefab3, prefab4;
+    public GameObject prefab;
+    public float spawnX;
+    public float spawnY1, spawnY2, spawnY3, spawnY4;
+    public float spawnRate = 150f;
+    float nextSpawnDistance = 100f;
+    float nextSpawnTime = 0f;
+    int whatToSpawn;
+
+    void Start()
+    {
+        spawnX = (Camera.main.pixelWidth * 2) + 300f;
+        spawnY1 = Camera.main.pixelHeight - 155f;
+        spawnY2 = Camera.main.pixelHeight - 155f;
+        spawnY3 = Camera.main.pixelHeight - 160f;
+        spawnY4 = Camera.main.pixelHeight - 120f;
+    }
+
+    void Update()
+    {
+        if (Camera.main.transform.position.x > nextSpawnDistance / 2 && Time.time > nextSpawnTime / 2)
+        {
+            whatToSpawn = Random.Range(1, 5);
+
+            switch (whatToSpawn)
+            {
+                case 1:
+                    prefab = Instantiate(prefab1, new Vector3(spawnX, spawnY1, 0), Quaternion.identity);
+                    break;
+                case 2:
+                    prefab = Instantiate(prefab2, new Vector3(spawnX, spawnY2, 0), Quaternion.identity);
+                    break;
+                case 3:
+                    prefab = Instantiate(prefab3, new Vector3(spawnX, spawnY3, 0), Quaternion.identity);
+                    break;
+                case 4:
+                    prefab = Instantiate(prefab4, new Vector3(spawnX, spawnY4, 0), Quaternion.identity);
+                    break;
+            }
+
+            Debug.Log(prefab.transform.position.x);
+            spawnX += 900f;
+            nextSpawnDistance = Camera.main.transform.position.x + spawnX;
+            nextSpawnTime = Time.time + spawnRate;
+        }
+    }
+}
